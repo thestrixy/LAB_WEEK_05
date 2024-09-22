6b6fcf8db323c1dd963635f8675bca5554096007
Koongh <yapkng@gmail.com>
2024-09-23 04:22:25 +0700

    login regist nav

--- app/src/main/java/com/example/lab_week_05/LoginFragment.kt
+++ app/src/main/java/com/example/lab_week_05/LoginFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -35,6 +37,16 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        (activity as MainActivity).hideBottomNavigation()
+        val registerButton = view.findViewById<Button>(R.id.regist_button)
+
+        registerButton.setOnClickListener{
+            findNavController().navigate(R.id.registerFragment)
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.lab_week_05
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -8,6 +9,7 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNavigation: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,7 +17,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
-        findViewById<BottomNavigationView>(R.id.bottom_nav)?.setupWithNavController(navController)
+        bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigation?.setupWithNavController(navController)
+        bottomNavigation.visibility = View.VISIBLE
+    }
+
+    fun hideBottomNavigation() {
+        bottomNavigation.visibility = View.GONE
     }
 
 //    override fun onSupportNavigateUp(): Boolean {
